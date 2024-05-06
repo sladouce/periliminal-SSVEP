07a333dc5762c1cdd96cb6a9de1e1eb49915593d
Simon Ladouce <38053484+sladouce@users.noreply.github.com>
2024-05-06 22:01:36 +0200

    Add files via upload

---
+++ PlotAxisAtCue.m
@@ -0,0 +1,26 @@
+function PlotAxisAtOrigin
+%PlotAxisAtOrigin Plot 2D axes through the origin
+
+% GET TICKS
+X=get(gca,'Xtick');
+Y=get(gca,'Ytick');
+
+% GET LABELS
+XL=get(gca,'XtickLabel');
+YL=get(gca,'YtickLabel');
+
+% GET OFFSETS
+Xoff=diff(get(gca,'XLim'))./60;
+Yoff=diff(get(gca,'YLim'))./60;
+
+% DRAW AXIS LINEs
+plot(get(gca,'XLim'),[0 0],'k');
+plot([0 0],get(gca,'YLim'),':k');
+
+% Plot new ticks  
+
+
+box off;
+% axis square;
+axis off;
+set(gcf,'color','w');
